9b02a7f5ac836c06433ff48540836bc4a6cf0859
dreamworkshop9-crypto <dreamworkshop9@gmail.com>
2026-03-20 20:54:24 +0700

    upload

---
+++ resources/views/list_barang.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>List Barang</title>
+</head>
+<body>
+
+<h1>Data Barang</h1>
+
+<p>ID Barang : {{ $id }}</p>
+<p>Nama Barang : {{ $nama }}</p>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,39 @@
 <?php
 
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+Route::get('/user/{id}', function ($id) {
+    return "User ID : " . $id;
+});
+
+Route::prefix('admin')->group(function () {
+
+    Route::get('/dashboard', function () {
+        return "Halaman Admin Dashboard";
+    });
+
+    Route::get('/users', function () {
+        return "Halaman Admin Users";
+    });
+
+});
+
+Route::get('/listbarang/{id}/{nama}', function ($id, $nama) {
+    return view('list_barang', [
+        'id' => $id,
+        'nama' => $nama
+    ]);
+});
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/listbarang/{id}/{nama}', function ($id, $nama) {
+    return view('list_barang', [
+        'id' => $id,
+        'nama' => $nama
+    ]);
+
+Route::get('/listbarang/{id}/{nama}', [ListBarangController::class, 'show']);
 });
